7eae8e3f4cc44aa271b8c82437bd415d43a80f9f
Lobana Ibrahim <Lobana.ibrahim@gmail.com>
2019-08-23 21:41:34 +0200

    first-to do

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,16 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+    it('Should Have a URL', function() {
+            var feed;
+            
+            for(var i = 0; i < allFeeds.length; i++) {
+                feed = allFeeds[i];
+                
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBe('');
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
